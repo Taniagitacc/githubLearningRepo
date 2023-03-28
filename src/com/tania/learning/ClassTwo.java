6650f4362a0172740282d4b43f5df81c4325fc81
taniagitacc <taniapatwary2014@gmail.com>
2023-03-28 17:31:15 -0400

    second class is created

---
+++ src/com/tania/learning/ClassTwo.java
@@ -0,0 +1,11 @@
+package com.tania.learning;
+
+public class ClassTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tania is leaning Github and pushing this code to github");
+		
+	}
+
+}
