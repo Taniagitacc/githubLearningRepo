8f4bde897364a00507679b0836e0108c60788ee7
taniagitacc <taniapatwary2014@gmail.com>
2023-03-28 17:26:58 -0400

    Java project is created

---
+++ src/com/tania/learning/LearningJavaClass.java
@@ -0,0 +1,14 @@
+package com.tania.learning;
+
+public class LearningJavaClass {
+
+	public static void main(String[] args) {
+		
+		
+		
+	System.out.println("Tania is leaning Github and pushing this code to github");
+	
+	
+	}
+
+}
